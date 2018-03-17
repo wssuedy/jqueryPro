31d8f0828f0f25352dd49c9007f3670110cf45b8
huht <huht@asiainfo.com>
2018-03-17 21:12:08 +0800

    init

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require("fs");
+var {promisify} = require("util");
+var writeFile = promisify(fs.writeFile);
+//把 fs写入 writeFile 的回调函数转化成 promise对象的一个工具
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+// app.use('/users', users);
+
+const db = require("./db");
+
+app.get("/init_data",function(req,res){
+  res.send(db);
+})
+
+app.post("/write",async function(req,res){
+  db.push(req.body);//未真实的写入文件db.json
+  await writeFile(__dirname+"/db.json",JSON.stringify(db));//写入 返回的是promise 可以await
+  res.send();
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
